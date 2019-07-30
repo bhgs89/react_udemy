9c1916537a12a5925906c5249b54a607f51a88e2
Han <han.baek.program@gmail.com>
2019-07-29 23:18:33 -0700

    Break point

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,64 +1,28 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
-
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
+import StreamForm from './StreamForm';
 // Field is Component
 // reduxForm is function
 
 class StreamCreate extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
   };
-
-  onSubmit(formValue) {
-    console.log(formValue);
-  }
 
   // handleSubmit은 자동으로 preventDefault() 함수를 포함한다.
   // 또한 event parameter를 자동으로 받는다
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field name="description" component={this.renderInput} label="Enter Description" />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = formValues => {
-  const errors = {};
-
-  if (!formValues.title) {
-    // only ran if the user did not enter a title
-    errors.title = 'You must enter a title';
-  }
-
-  if (!formValues.description) {
-    errors.description = 'You must enter a description';
-  }
-
-  return errors;
-};
-
-export default reduxForm({
-  form: 'streamCreate',
-  validate,
-})(StreamCreate);
+export default connect(
+  null,
+  { createStream },
+)(StreamCreate);
---
+++ streams/client/src/components/streams/StreamForm.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+// Field is Component
+// reduxForm is function
+
+class StreamForm extends React.Component {
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  onSubmit = formValues => {
+    console.log(this.props);
+    this.props.onSubmit(formValues);
+  };
+
+  // handleSubmit은 자동으로 preventDefault() 함수를 포함한다.
+  // 또한 event parameter를 자동으로 받는다
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field name="description" component={this.renderInput} label="Enter Description" />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    // only ran if the user did not enter a title
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: 'streamForm',
+  validate,
+})(StreamForm);
