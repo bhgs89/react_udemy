f71a13ff43e830ce22cf8a1514360d6e08ee5ddd
bhgs89 <han.baek.program@gmail.com>
2019-08-01 02:46:36 -0700

    Complete Section 24 - From Redux to Context

--- translate/src/contexts/LanguageContext.js
+++ translate/src/contexts/LanguageContext.js
@@ -1,3 +1,25 @@
 import React from 'react';
 
-export default React.createContext('dutch');
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component {
+  // Context Store
+  state = { language: 'english' };
+
+  // Manipulate Store data
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <Context.Provider
+        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+      >
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default Context;
